91fbe511f2497300ea2aac5e784915bd054b1bd8
TyrionSnow1911 <glm3531@gmail.com>
2021-08-13 17:25:38 -0600

    Add more styling to home component html. Add initial variables to home component ts.

--- vendingMachine/src/app/home/home.component.ts
+++ vendingMachine/src/app/home/home.component.ts
@@ -1,13 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
-
+import { Injector } from '@angular/core';
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  // class variables
+  cents: string = '';
+  pennys: string = '';
+  nickles: string = '';
+  quarters: string = '';
+  dimes: string = '';
+
+  cokeQuantityAvailable: number = 5;
+  pepsiQuantityAvailable: number = 15;
+  sodaQuantityAvailable: number = 3;
+  cokeCost: number = 25;
+  pepsiCost: number = 36;
+  sodaCost: number = 45;
+  ordetTotalCost: number = 0;
+
   // form variables
   centsForm = new FormGroup({
     cents: new FormControl(''),
@@ -29,7 +44,24 @@ export class HomeComponent implements OnInit {
     pepsi: new FormControl(''),
   });
 
-  constructor() {}
+  constructor(private injector: Injector) {}
 
   ngOnInit(): void {}
+
+  getDrinks() {}
+  getCokeQuantityAvailable() {
+    document.getElementsByClassName('coke-quantity');
+  }
+  getPepsiQuantityAvailable() {}
+  getSodaQuantityAvailable() {}
+
+  getCokeOrder() {}
+  getPepsiOrder() {}
+  getSodaOrder() {}
+
+  getCents() {}
+  getPennys() {}
+  getNickles() {}
+  getQuarters() {}
+  calculateOrderTotal() {}
 }
